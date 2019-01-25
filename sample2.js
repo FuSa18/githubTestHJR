d85a2efb307f0d6e65f62ba31dce3851d936e994
FuSa18 <Inari.fukasan79@gmail.com>
2019-01-25 20:04:14 +0900

    create button and function

---
+++ sample2.js
@@ -0,0 +1,13 @@
+function fncCall() {
+  var mes = "関数が呼び出されました。";
+  alert(mes);
+}
+/* グローバル変数
+var number = 1;
+
+function fnc1() {
+  // ローカル変数
+  var number2 = 1;
+  alert("number: "+number);
+  alert("number2: "+number2);
+}*/
