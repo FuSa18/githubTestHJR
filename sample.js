5513ef88cf5e6971c0f2e26497735784c2b6a650
FuSa18 <sei.e5hayabusa@gmail.com>
2019-01-25 19:01:11 +0900

    create a sample.js

---
+++ sample.js
@@ -0,0 +1,35 @@
+// HelloWorld!!という警告ウインドウを表示する(コメントアウトはctrl+/)
+alert("HelloWorld!!");
+// 2+3を警告ウインドウで表示する
+alert(2+3);
+//文字と数値を結合して文字列として表示する
+alert(6+"月");
+//文字列を改行して表示させる
+alert("円周率は\n"+3.14159);
+//サンプルの結果を表示したりできる
+document.write("javascriptは有効です"+"<br>");
+//htmlタグも使用できる
+document.write(" <strong>10+20</strong>の答えは");
+document.write(10+20);
+document.write("です！");
+//変数(以下のように宣言する)
+// var fruits;
+// var vegetables, drinks, meats;
+// var apple = 5;
+var num1 = 10; //プログラムでの=は代入を示す(右辺を左辺に代入する)
+var num2 = 5;
+alert(num1+num2); //足し算
+alert(num1-num2); //引き算
+alert(num1*num2); //掛け算
+alert(num1/num2); //割り算
+alert(num1%num2); //余り
+//文字列の連結は変数でもできる
+var num_koyomi = 7;
+var str = "月";
+alert(num_koyomi+str);
+// 定数(以下のように宣言する)
+// const fruits;
+// const vegetables, drinks, meats;
+// const apple = 5;
+const num3 = "Tom";
+alert(num3);
